d5c909a0b266515e17e03259e04e8baadbcc52e9
Robb Shecter <robb@public.law>
2022-10-12 19:27:39 -0600

    add Result

--- src/functional_adt_patterns.py
+++ src/functional_adt_patterns.py
@@ -11,16 +11,19 @@
 
 
 from dataclasses import dataclass
-import datetime
 from typing import Any, Callable, TypeAlias
+import datetime
 
 from order_status_adt import OrderStatus_6, Ready, Scheduled, Shipped
 
 
+#
+# Dynamically typed Maybe
+#
+
 @dataclass
 class Nothing:
     pass
-
 
 @dataclass
 class Just:
@@ -29,7 +32,6 @@ class Just:
 
 Maybe: TypeAlias = Just | Nothing
 
-maybe: Callable[[Any], Maybe] = lambda x: Just(x) if x is not None else Nothing()
 
 
 def order_date(status: OrderStatus_6) -> Maybe:
@@ -41,7 +43,74 @@ def order_date(status: OrderStatus_6) -> Maybe:
             return Just(on)
 
 
+# Idea: Maybe constructor. Wraps a value in a Maybe.
+#       Not used in this example.
+# maybe: Callable[[Any], Maybe] = lambda x: Just(x) if x is not None else Nothing()
+
+
+# Result type
+
+@dataclass
+class Ok:
+    value: Any
+
+    def unwrap(self):
+        return self.value
+
+@dataclass
+class Err:
+    error: Any
+
+    def unwrap(self):
+        raise Exception(self.error)
+
+
+Result: TypeAlias = Ok | Err
+
+
+def order_date_2(status: OrderStatus_6) -> Result:
+    match (status):
+        case Ready() | "unknown" | "not available":
+            return Err("No date, unknown, or not available")
+
+        case Scheduled(on) | Shipped(on):
+            return Ok(on)
+
+
+def time_since_order(status: OrderStatus_6) -> Result:
+    match (order_date_2(status)):
+        case Ok(date):
+            return Ok(datetime.date.today() - date)
+
+        case Err(error):
+            return Err(error)
+
+
+def time_since_order_2(status: OrderStatus_6) -> datetime.timedelta:
+   return datetime.date.today() - order_date_2(status).unwrap()
+
+
 
 if __name__ == "__main__":
-    print(order_date(Ready()))
-    print(order_date(Scheduled(datetime.date(2021, 1, 1))))
+
+    # Enforced error handling with Maybe
+    match (order_date(Ready())):
+        case Just(date):
+            print(f"Order date is {date}")
+        case Nothing():
+            print("No order date")
+
+    # Enforced error handling with Result
+    match(order_date_2(Ready())):
+        case Ok(date):
+            print(f"Order date is {date}")
+        case Err(error):
+            print(f"Error: {error}")
+
+    match(time_since_order(Ready())):
+        case Ok(time_since):
+            print(f"Time since order: {time_since}")
+        case Err(error):
+            print(f"Error: {error}")
+
+    print("Time since order: ", time_since_order_2(Ready()))
