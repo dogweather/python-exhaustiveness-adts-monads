72b10126590f8d2b419827f37a8c81d53d28504b
Robb Shecter <robb@public.law>
2022-10-12 20:05:09 -0600

    ADTs and Monads (#2)
    * first pass at ADTs
    
    * add Result
    
    * unwrap and unwrap_or
    
    * monad

---
+++ src/adt_2_result.py
@@ -0,0 +1,132 @@
+
+#
+# Implementing some functional patterns with ADTs
+#
+#
+# Wishlist
+#
+# * Make match an expression. This would save having to repeat
+#   a variable name when assigning.
+
+
+
+from dataclasses import dataclass
+from typing import Any, TypeAlias
+import datetime
+
+from adt_1_order_status import OrderStatus_6, Ready, Scheduled, Shipped
+
+
+# Result type
+
+@dataclass
+class Ok:
+    value: Any
+
+    def is_ok(self) -> bool:
+        return True
+
+    def unwrap(self):
+        return self.value
+
+    def unwrap_or(self, _: Any) -> Any:
+        return self.value
+
+@dataclass
+class Err:
+    error: Any
+
+    def is_ok(self) -> bool:
+        return False
+
+    def unwrap(self):
+        raise Exception(self.error)
+
+    def unwrap_or(self, default: Any) -> Any:
+        return default
+
+
+Result: TypeAlias = Ok | Err
+
+
+def order_date_2(status: OrderStatus_6) -> Result:
+    match (status):
+        case Ready() | "unknown" | "not available":
+            return Err("No date, unknown, or not available")
+
+        case Scheduled(on) | Shipped(on):
+            return Ok(on)
+
+
+def time_since_order(status: OrderStatus_6) -> Result:
+    match (order_date_2(status)):
+        case Ok(date):
+            return Ok(datetime.date.today() - date)
+
+        case Err(error):
+            return Err(error)
+
+
+def time_since_order_2(status: OrderStatus_6) -> datetime.timedelta:
+   return datetime.date.today() - order_date_2(status).unwrap()
+
+
+
+if __name__ == "__main__":
+
+    # Enforced error handling with Maybe
+    match (order_date(Ready())):
+        case Just(date):
+            print(f"Order date is {date}")
+        case Nothing():
+            print("No order date")
+
+    # Enforced error handling with Result
+    match(order_date_2(Ready())):
+        case Ok(date):
+            print(f"Order date is {date}")
+        case Err(error):
+            print(f"Error: {error}")
+
+    match(time_since_order(Ready())):
+        case Ok(time_since):
+            print(f"Time since order: {time_since}")
+        case Err(error):
+            print(f"Error: {error}")
+
+    print("Time since order: ", time_since_order_2(Ready()))
+
+    date = order_date_2(Ready())
+
+    # Fails typecheck because the type checking points out that
+    # Err doesn't have a `value` attribute.
+    print(date.value)
+
+    # Likewise, `date` may not have the error attribute.
+    print(date.error)
+
+    #
+    # The type of `date` *must* be checked before accessing.
+    # These succeed the typecheck because we handle the error case.
+    #
+
+    # Return the given default value on failure.
+    print(date.unwrap_or("No date"))
+
+    # Crash the application on failure.
+    print(date.unwrap())
+
+    # Pyright can't infer `date`'s type throught the `if`. 
+    # This code doesn't typecheck.
+    if date.is_ok():
+        print(date.value)
+    else:
+        print(date.error)
+
+    # Instead, we can use a match expression to handle the
+    # error case. Pyright can do this flow analysis and type-narrowing.
+    match (date):
+        case Ok(date):
+            print(date)
+        case Err(error):
+            print(error)
---
+++ src/adt_3_result_monad.py
@@ -0,0 +1,63 @@
+
+#
+# Implementing some functional patterns with ADTs
+# This example makes Result into a Monad: it prevents
+# direct access to its wrapped value.
+#
+
+
+from dataclasses import dataclass
+from typing import Any, TypeAlias
+
+
+
+
+# Result type, now as a Monad. (I think! It pretty much follows
+# the Monad pattern.)
+
+
+@dataclass(frozen=True)
+class Ok:
+    __value: Any  # Private for extra Monad-style safety
+
+    def is_ok(self) -> bool:
+        return True
+
+    def unwrap(self):
+        return self.__value
+
+    def unwrap_or(self, _: Any) -> Any:
+        return self.__value
+
+@dataclass(frozen=True)
+class Err:
+    __error: Any  # Private for extra Monad-style safety
+
+    def is_ok(self) -> bool:
+        return False
+
+    def unwrap(self):
+        raise Exception(self.__error)
+
+    def unwrap_or(self, default: Any) -> Any:
+        return default
+
+
+Result: TypeAlias = Ok | Err
+
+
+
+if __name__ == "__main__":
+    #
+    # Demonstrating extra safety from the private attributes.
+    #
+    result = Ok("Success")
+
+    # Fails typecheck
+    print(result.value)
+
+    # Fails typecheck
+    print(result.__value)
+
+    # Passes typecheck
+    value = result.unwrap_or("Default")
